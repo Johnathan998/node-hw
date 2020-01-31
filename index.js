fbf56532bb95bffcc1b331e335088502fc369f70
johnathan olivas <jj.galvan13@yahoo.com>
2020-01-30 22:30:33 -0700

    grabbing repsonse from api

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ inquirer
     {
         message: "What is your favorite color?",
         name: "color"
-    }])
+    }]).then(function ({ username, color }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+
+        axios.get(queryUrl)
+            .then(function (response) {
+                const picUrl = response.data.avatar_url;
+                const location = response.data.location;
+                const gitHubUrl = response.data.html_url;
+                const blog = response.data.blog;
+                const bio = response.data.bio;
+                const numRepos = response.data.public_repos;
+                const numFollowers = response.data.followers;
+                const numFollowing = response.data.following;
+                let numStarred = 0;
