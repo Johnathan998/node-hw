8810d0dd3f5513caef56df13f487d6d0a6407025
johnathan olivas <jj.galvan13@yahoo.com>
2020-01-30 22:16:16 -0700

    added prompt

---
+++ index.js
@@ -0,0 +1,9 @@
+inquirer
+    .prompt([{
+        message: "Enter GitHub username",
+        name: "username"
+    },
+    {
+        message: "What is your favorite color?",
+        name: "color"
+    }])
