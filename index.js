49886c4df0c61f537444cfb1efd8824f6be4832b
johnathan olivas <jj.galvan13@yahoo.com>
2020-01-30 22:43:42 -0700

    creating profilr html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-nquirer
+inquirer
     .prompt([{
         message: "Enter GitHub username",
         name: "username"
@@ -10,7 +10,8 @@ nquirer
     {
         message: "What is your favorite color?",
         name: "color"
-    }]).then(function ({ username, color }) {
+    }])
+    .then(function ({ username, color }) {
         const queryUrl = `https://api.github.com/users/${username}`;
 
         axios.get(queryUrl)
@@ -25,6 +26,92 @@ nquirer
                 const numFollowing = response.data.following;
                 let numStarred = 0;
 
+
                 axios.get(`https://api.github.com/users/${username}/starred`)
                     .then(function (response) {
                         numStarred = response.data.length;
+                        const newHtml = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>${username}'s Resume</title>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            </head>
+            <body>
+                <div class="container">
+                    <div class="row my-5">
+                        <div class="col-12">
+                            <div class="card text-center">
+                                <img src="${picUrl}" class="card-img-top mx-auto rounded-circle border border-warning" alt="Profile picture" style="width: 18rem;">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 36px;">${username}</p>
+                                    <p class="card-text" style="font-size: 24px;">${bio}</p>
+                                    <a class="card-link" href="https://google.com/maps/place/${location}">${location}</a>
+                                    <a class="card-link" href="${gitHubUrl}">GitHub</a>
+                                    <a class="card-link" href="${blog}">Blog</a>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Public Repositories</p>
+                                    <p class="card-text" style="font-size: 20px;">${numRepos}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Followers</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowers}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">GitHub Stars</p>
+                                    <p class="card-text" style="font-size: 20px;">${numStarred}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Following</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowing}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <style type="text/css">
+                    body {
+                    background-color: white;
+                    -webkit-print-color-adjust: exact !important;
+                    }
+                    .card {
+                        background-color: ${color};
+                        color: white;
+                    }
+                    .card-link {
+                        color: white;
+                    }
+                </style>
+            </body>
+            </html>`;
+
+
+                        fs.writeFile("profile.html", newHtml, function (err) {
+                            if (err) throw err;
+                        });
+                    });
+            });
+    });
